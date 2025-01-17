8193d79deeb740d464a8d6e6bf36d2540252dd8f
nsandrez <n.orrego@outlook.cl>
2025-01-16 21:49:44 -0300

    agregando ruta camion

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import usuarioRouter from '../app/routes/usuarioRoutes';
 import proyectoRouter from '../app/routes/proyectoRoutes';
-import proyectoUsuarioRoutes from '../app/routes/proyectoUsuarioRoutes';
-
+import proyectoUsuarioRouter from '../app/routes/proyectoUsuarioRoutes';
+import camionRouter from '../app/routes/camionRoutes';
 
 
 const app = express();
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.json()); // Middleware para manejar JSON
 app.use('/api/usuarios', usuarioRouter); // Conecta las rutas de usuarios
 app.use('/api/proyectos', proyectoRouter); // Conecta las rutas de proyectos
-app.use('/api/proyectos-usuarios', proyectoUsuarioRoutes); // Conecta las rutas de relación usuario-proyecto
+app.use('/api/proyectos-usuarios', proyectoUsuarioRouter); // Conecta las rutas de relación usuario-proyecto
+app.use('/api/camiones', camionRouter); // Conecta las rutas de camion
+
 
 export default app; // Exporta la instancia de app como default
